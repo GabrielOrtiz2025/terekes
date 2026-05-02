170120788c9568a3aeb18e79bee108a87468c614
GabrielOrtiz2025 <jgabrielodg86@gmail.com>
2026-05-01 20:40:47 -0500

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,20 +1,58 @@
 const productos = [
+
 {
-    nombre: "Pijama Premium",
+    nombre: "Pijama Negra",
+    desc: "Algodón premium / Unisex",
+    categoria: "pijamas",
     imgs: [
-    "img/img1.jpg"
-],
-    colores: ["Negro","Blanco","Rojo"],
-    tallas: ["S","M","L"],
-    size: "big"
+        "img/img1.jpg"
+    ],
+    colores: ["Negro","Blanco"],
+    tallas: ["S","M","L"]
 },
+
 {
-    nombre: "Accesorio X",
-   imgs: [
-    "img/img1.jpg"
-],
-    colores: ["Negro","Gris"],
-    tallas: ["Única"],
-    size: "medium"
+    nombre: "Accesorio Demo",
+    desc: "Producto de prueba",
+    categoria: "accesorios",
+    imgs: [
+        "https://via.placeholder.com/483x604"
+    ],
+    colores: ["Negro"],
+    tallas: ["Única"]
+},
+
+{
+    nombre: "Mug Demo",
+    desc: "Cerámica",
+    categoria: "mugs",
+    imgs: [
+        "https://via.placeholder.com/483x604"
+    ],
+    colores: ["Blanco"],
+    tallas: ["Única"]
+},
+
+{
+    nombre: "Camiseta Demo",
+    desc: "100% algodón",
+    categoria: "camisetas",
+    imgs: [
+        "https://via.placeholder.com/483x604"
+    ],
+    colores: ["Negro"],
+    tallas: ["S","M","L"]
+},
+
+{
+    nombre: "Hoodie Demo",
+    desc: "Oversize",
+    categoria: "hoodies",
+    imgs: [
+        "https://via.placeholder.com/483x604"
+    ],
+    colores: ["Negro"],
+    tallas: ["M","L"]
 }
+
 ];
